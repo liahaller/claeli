4ca837cbe407d8bc5b5de56533eeb69fa00a9185
Clara Salomao <clarans@al.insper.edu.br>
2025-04-29 15:32:28 -0300

    guardar_dado

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,12 @@ def rolar_dados(n):
     for i in range(n):
         dados.append(random.randint(1,6))
     return dados
+
+def guardar_dado(dados_rolados, dados_guardados, numero):
+    nova=[]
+    dados_guardados.append(dados_rolados[numero])
+    for i in range(len(dados_rolados)):
+        if i != numero:
+            nova.append(dados_rolados[i])
+    return nova, dados_guardados
+
