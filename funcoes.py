4f900f5159afd74161e55f32064bc1687e1b4bff
Lia Haller <liahv@al.insper.edu.br>
2025-04-29 15:56:21 -0300

    remover_dado

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,10 @@ def guardar_dado(dados_rolados, dados_guardados, numero):
             nova.append(dados_rolados[i])
     return [nova, dados_guardados]
 
+def remover_dado(dados_rolados,dados_guardados,numero):
+    dados_guardados_novo = []
+    dados_rolados.append(dados_guardados[numero])
+    for i in range(len(dados_guardados)):
+        if i != numero:
+            dados_guardados_novo.append(dados_guardados[i])
+    return[dados_rolados,dados_guardados_novo]
