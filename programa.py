eb72714f3781536f07cff1e69189833d7f328bcf
Lia Haller <liahv@al.insper.edu.br>
2025-04-30 16:55:32 -0300

    combinacao

--- programa.py
+++ programa.py
@@ -52,8 +52,8 @@ while rodadas<12:
             dados_totais.append(dados_rolados[i])
         combinacao= input("Digite a combinação desejada:")
         achou = 0
-        if len(categoria) == 1:
-            categoria = int(categoria)
+        if len(combinacao) == 1:
+            combinacao = int(combinacao)
         for tipo in cartela_de_pontos.keys():
             if combinacao in cartela_de_pontos[tipo]:
                 regra = tipo
